9aa2641f49f38a3e0c776955983f8b299d4faccc
North077 <deykiron365@gmail.com>
2024-03-04 21:50:01 +0530

    Setup lil-gui

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,11 @@
 import './style.css';
 import { Renderer } from '@/Renderer';
 import { EffectComposer, BlurPass, BloomPass } from '@/PostProcessing';
+import GUI from 'lil-gui';
 
 const canvas = <HTMLCanvasElement>document.getElementById('renderer')!;
 
-const constants = {
-	a: { value: 3.0 }
-};
-
-const renderer = new Renderer(canvas, { constants });
+const renderer = new Renderer(canvas);
 
 const composer = new EffectComposer();
 composer.addPass(new BlurPass());
@@ -16,4 +13,32 @@ composer.addPass(new BloomPass());
 
 renderer.setEffectComposer(composer);
 renderer.render();
-renderer.render();
+
+function animate() {
+	requestAnimationFrame(animate);
+	renderer.render();
+}
+
+animate();
+
+setupGUI();
+
+function setupGUI() {
+	const { constants } = renderer.config;
+	const gui = new GUI();
+	const folder = gui.addFolder('contants');
+	const numerator = folder.addFolder('numerator');
+
+	['a', 'b', 'c', 'd', 'e'].forEach((key) => {
+		numerator
+			.add(constants[key as keyof typeof constants], 'value', -6, 6)
+			.name(key);
+	});
+
+	const denominator = folder.addFolder('numerator');
+	['f', 'g', 'h', 'i', 'j'].forEach((key) => {
+		denominator
+			.add(constants[key as keyof typeof constants], 'value', -6, 6)
+			.name(key);
+	});
+}
